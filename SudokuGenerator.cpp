f5aa7c79433b5457cfd17b2e3f3c73dc4134f492
Momir Stanišić <momir.stanisic.ns@gmail.com>
2023-12-30 14:45:53 +0100

    Added generator

--- SudokuGenerator.cpp
+++ SudokuGenerator.cpp
@@ -1,1 +1,33 @@
 #include "SudokuGenerator.h"
+
+template<unsigned int N> std::array<int, N> SudokuGenerator::getRandomArray(int start) {
+	std::random_device rd = std::random_device{};
+	std::mt19937 rng = std::default_random_engine{ rd() };
+	std::array<int, N> values;
+	std::iota(values.begin(), values.end(), start);
+	std::shuffle(values.begin(), values.end(), rng);
+	return values;
+}
+
+void SudokuGenerator::fillBox(int box) {
+	std::array values = getRandomArray<9>(1);
+	for (int i = 0; i < 3 ; i++)
+		for (int j = 0; j < 3; j++)
+			table.setValue(box * 3 + i, box * 3 + j, values[i * 3 + j]);
+}
+
+Sudoku9 SudokuGenerator::generate(int emptyCells, int puzzleNo) {
+	table = Sudoku9(puzzleNo);
+
+	for (int i = 0; i < 3; i++)
+		fillBox(i);
+
+	SudokuSolver solver(table);
+	table = solver.getSolution();
+
+	std::array pos = getRandomArray<81>();
+	for (int i = 0; i < emptyCells; i++)
+		table.setValue(pos[i] / 9, pos[i] % 9, 0);
+
+	return table;
+}
--- SudokuGenerator.h
+++ SudokuGenerator.h
@@ -1,4 +1,19 @@
 #pragma once
+#include "SudokuSolver.h"
+#include "Sudoku9.h"
+#include <algorithm>
+#include <numeric>
+#include <random>
+#include <array>
+
 class SudokuGenerator {
+private:
+	Sudoku9 table;
+
+	template <unsigned int N> std::array<int, N> getRandomArray(int start = 0);
+	void fillBox(int box);
+
+public:
+	Sudoku9 generate(int emptyCells = 33, int puzzleNo = 0);
 };
 
